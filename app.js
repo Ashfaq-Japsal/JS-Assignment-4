314eea8a85b8d5c1614c076918d16a911c6bb141
Ashfaq <mybusinessisduty482@gmail.com>
2023-11-13 12:47:31 +0500

    4th assignment

---
+++ app.js
@@ -0,0 +1,30 @@
+//1
+var var1 = "I am var1,"
+var var2 = "I am var2,"
+var var3 = "I am var3."
+alert (var1 + " " + var2 + " " + var3)
+//2
+// 5 legal  vars
+var myName = ""
+var myDOB1 = ""
+var my_Age = ""
+var myCollege$ = ""
+var myCourse = ""
+// 5 illegal vars
+// var my Name = ""
+// var 1myDOB = ""
+// var my-Age = ""
+// var myCollege? = ""
+// var !myCourse = ""
+//3
+document.write ("<h1>Rules for naming JS variables</h1>")
+document.write ("<br>")
+document.write ("<br>")
+document.write ("<br>")
+document.write ("Variable names can only contain numbers, $ and _. For example: $my_1stVariable")
+document.write ("<br>")
+document.write ("Variables must begin with a letter, $ or _. For example $name, _name or name")
+document.write ("<br>")
+document.write ("Variable names are case sensitive")
+document.write ("<br>")
+document.write ("Variable names should not be JS keywords")
